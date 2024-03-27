16d80350e00898552e89280f7eb19d4a29686cd4
kimsomi0126 <kimsomi00126@gmail.com>
2024-03-27 10:14:11 +0900

    feat : GA4 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 // index.html 역할
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,15 +13,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
-        <meta
-          name="naver-site-verification"
-          content="18932c563ca8c7600147e0f60244307d7b421797"
-        />
         <link rel="canonical" href="https://kkobrain-six.vercel.app"></link>
-        <meta
-          name="google-site-verification"
-          content="Q2bHOLs6jaFYy4NK_gCCdUCjf270wQ8zYE_nerDd7lw"
-        />
+        <GoogleAnalytics gaId="G-KS5PJDPT1S" />
       </head>
       <body>
         <noscript>You need to enable JavaScript to run this app.</noscript>
